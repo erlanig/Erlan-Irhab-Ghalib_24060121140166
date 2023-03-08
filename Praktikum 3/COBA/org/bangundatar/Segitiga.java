c01b89e5036ff5339b4e71d85e563327ffb7c4f2
erlanig <89430452+erlanig@users.noreply.github.com>
2023-03-08 20:07:00 +0700

    Add files via upload

---
+++ Praktikum 3/COBA/org/bangundatar/Segitiga.java
@@ -0,0 +1,29 @@
+/**
+ * File      : Segitiga.java		08/03/23
+ * Penulis   : Erlan Irhab Ghalib
+ * Deskripsi : Representasi dasar dari objek segitiga, turunan kelas poligon
+ * 
+ */
+
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+    private double alas;
+    private double tinggi;
+
+    public Segitiga(double alas, double tinggi, int jumlahSisi) {
+        this.jumlahSisi = jumlahSisi;
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    public double hitungLuas(){
+        return (alas * tinggi) / 2;
+    }
+
+    public void printInfo(){
+        System.out.println("Bangun Segitiga bersisi "+ this.getJumlahSisi());
+    }
+}
